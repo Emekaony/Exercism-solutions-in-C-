0eea91beac601dea055a94b979160147ad21ef48
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-04-17 21:03:51 -0700

    Worked on both functions

--- phone-number-analysis/PhoneNumberAnalysis.cs
+++ phone-number-analysis/PhoneNumberAnalysis.cs
@@ -1,14 +1,16 @@
-using System;
-
 public static class PhoneNumber
 {
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+        bool IsNewYork = phoneNumber.IndexOf("212") == 0;
+        bool IsFake = phoneNumber[4..7] == "515";
+        string LocalNumber = phoneNumber[8..];
+
+        return (IsNewYork, IsFake, LocalNumber);
     }
 
     public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
+        return phoneNumberInfo.IsFake;
     }
 }
