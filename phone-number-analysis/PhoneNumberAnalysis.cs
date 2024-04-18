15baf9cbe230c147a36af71e80537ad7403e1dd7
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-04-17 21:06:41 -0700

    Fixed some minor bugs

--- phone-number-analysis/PhoneNumberAnalysis.cs
+++ phone-number-analysis/PhoneNumberAnalysis.cs
@@ -2,11 +2,7 @@ public static class PhoneNumber
 {
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
-        bool IsNewYork = phoneNumber.IndexOf("212") == 0;
-        bool IsFake = phoneNumber[4..7] == "515";
-        string LocalNumber = phoneNumber[8..];
-
-        return (IsNewYork, IsFake, LocalNumber);
+        return (phoneNumber.StartsWith("212"), phoneNumber[4..7] == "555", phoneNumber[8..]);
     }
 
     public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
