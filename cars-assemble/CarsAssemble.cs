4a96c93d4bdafc86d456261689fc5ae95fd8d3b6
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-04-18 01:08:50 -0700

    Solved first milestone using a speed switch weird syntax

--- cars-assemble/CarsAssemble.cs
+++ cars-assemble/CarsAssemble.cs
@@ -4,9 +4,17 @@ static class AssemblyLine
 {
     public static double SuccessRate(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.SuccessRate() method");
+        return speed switch
+        {
+            0 => 0.00,
+            1 or 2 or 3 or 4 => 1.00,
+            5 or 6 or 7 or 8 => 0.90,
+            9 => 0.80,
+            10 => 0.77,
+            _ => 0.00,
+        };
     }
-    
+
     public static double ProductionRatePerHour(int speed)
     {
         throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
