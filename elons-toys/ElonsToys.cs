9ccfe4e80d6a25674f25dd493afb366144e1fa4f
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-04-19 12:50:51 -0700

    Just making the code look a lot cleaner.

--- elons-toys/ElonsToys.cs
+++ elons-toys/ElonsToys.cs
@@ -1,31 +1,16 @@
-using System;
-
 class RemoteControlCar
 {
     private int _batteryPercentage = 100;
     private int _distanceCovered = 0;
-    public static RemoteControlCar Buy()
-    {
-        return new RemoteControlCar();
-    }
+    public static RemoteControlCar Buy() => new();
 
-    public string DistanceDisplay()
-    {
-        return $"Driven {_distanceCovered} meters";
-    }
+    public string DistanceDisplay() => $"Driven {_distanceCovered} meters";
 
-    public string BatteryDisplay()
-    {
-        return _batteryPercentage == 0 ? "Battery empty" : $"Battery at {_batteryPercentage}%";
-    }
+    public string BatteryDisplay() => _batteryPercentage == 0 ? "Battery empty" : $"Battery at {_batteryPercentage}%";
 
     public void Drive()
     {
-        if (_batteryPercentage == 0)
-        {
-            Console.WriteLine("Unable to drive with battery below 0%");
-        }
-        else
+        if (_batteryPercentage > 0)
         {
             _batteryPercentage--;
             _distanceCovered += 20;
