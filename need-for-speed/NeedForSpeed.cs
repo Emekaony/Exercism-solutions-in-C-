08f601dff4d06faaa27a7074db6e9d8b9a501477
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-04-19 16:19:22 -0700

    Fixed Drive() method to check if battery is drained before driving.

--- need-for-speed/NeedForSpeed.cs
+++ need-for-speed/NeedForSpeed.cs
@@ -2,35 +2,73 @@ using System;
 
 class RemoteControlCar
 {
+    private int _speed;
+    private int _batteryDrain;
+    public int totalDistanceDriven = 0;
+    public int batteryLevel = 100;
+
     // TODO: define the constructor for the 'RemoteControlCar' class
+    public RemoteControlCar(int speed, int batteryDrain)
+    {
+        _speed = speed;
+        _batteryDrain = batteryDrain;
+    }
+
+    public int Speed
+    {
+        get => _speed;
+    }
 
     public bool BatteryDrained()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDrained() method");
+        return batteryLevel < _batteryDrain;
     }
 
     public int DistanceDriven()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDriven() method");
+        return totalDistanceDriven;
     }
 
     public void Drive()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+        if (BatteryDrained()) { }
+        else
+        {
+            totalDistanceDriven += _speed; // increase distance driven by the speed of the car
+            batteryLevel -= _batteryDrain; // decrement the battery level by the battery drain
+        }
     }
 
     public static RemoteControlCar Nitro()
     {
-        throw new NotImplementedException("Please implement the (static) RemoteControlCar.Nitro() method");
+        return new RemoteControlCar(50, 4);
     }
 }
 
 class RaceTrack
 {
+    public int _distance;
     // TODO: define the constructor for the 'RaceTrack' class
+    public RaceTrack(int distance)
+    {
+        _distance = distance;
+    }
 
     public bool TryFinishTrack(RemoteControlCar car)
     {
-        throw new NotImplementedException("Please implement the RaceTrack.TryFinishTrack() method");
+        int distanceToDrive = _distance;
+        bool canFinish = true;
+        while (distanceToDrive > 0)
+        {
+            if (car.BatteryDrained())
+            {
+                canFinish = false;
+                break;
+            }
+            car.Drive();
+            distanceToDrive -= car.Speed;
+
+        }
+        return canFinish;
     }
 }
