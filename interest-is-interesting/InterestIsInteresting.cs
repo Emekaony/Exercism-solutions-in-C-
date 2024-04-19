d612686c514d2faa15bba6b762baba24d946cfaa
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-04-19 11:34:58 -0700

    Fixed small year differenc ebug for desired balance

--- interest-is-interesting/InterestIsInteresting.cs
+++ interest-is-interesting/InterestIsInteresting.cs
@@ -4,21 +4,34 @@ static class SavingsAccount
 {
     public static float InterestRate(decimal balance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.InterestRate() method");
+        float rate = 0.0f;
+        if (balance < 0m)
+        {
+            rate = 3.213f;
+        }
+        else if (balance >= 0 && balance < 1000m)
+        {
+            rate = 0.5f;
+        }
+        else if (balance >= 1000m && balance < 5000m)
+        {
+            rate = 1.621f;
+        }
+        else if (balance >= 5000m)
+        {
+            rate = 2.475f;
+        }
+        return rate;
     }
 
-    public static decimal Interest(decimal balance)
-    {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.Interest() method");
-    }
+    public static decimal Interest(decimal balance) => balance * (decimal)(InterestRate(balance) / 100);
 
-    public static decimal AnnualBalanceUpdate(decimal balance)
-    {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.AnnualBalanceUpdate() method");
-    }
+    public static decimal AnnualBalanceUpdate(decimal balance) => balance + Interest(balance);
 
     public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.YearsBeforeDesiredBalance() method");
+        double top = Math.Log10(decimal.ToDouble(targetBalance / balance));
+        double bottom = Math.Log10(1 + InterestRate(balance) * 1 / 100);
+        return (int)Math.Ceiling(top / bottom);
     }
 }
