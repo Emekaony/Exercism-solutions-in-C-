2611ac981ff3fef9e15480caa612bb1666805b3d
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-04-19 18:38:49 -0700

    Cleaned up the code even more to meet new c# 8 standards

--- booking-up-for-beauty/BookingUpForBeauty.cs
+++ booking-up-for-beauty/BookingUpForBeauty.cs
@@ -2,19 +2,13 @@ using System;
 
 static class Appointment
 {
-    public static DateTime Schedule(string appointmentDateDescription)
-    {
-        return DateTime.Parse(appointmentDateDescription);
-    }
+    public static DateTime Schedule(string appointmentDateDescription) => DateTime.Parse(appointmentDateDescription);
 
     public static bool HasPassed(DateTime appointmentDate) => appointmentDate.CompareTo(DateTime.Now) < 0;
 
     public static bool IsAfternoonAppointment(DateTime appointmentDate) => appointmentDate.Hour >= 12 && appointmentDate.Hour < 18;
 
-    public static string Description(DateTime appointmentDate)
-    {
-        return $"You have an appointment on {DateTime.SpecifyKind(appointmentDate, DateTimeKind.Local)}.";
-    }
+    public static string Description(DateTime appointmentDate) => $"You have an appointment on {DateTime.SpecifyKind(appointmentDate, DateTimeKind.Local)}.";
 
-    public static DateTime AnniversaryDate() => new DateTime(DateTime.Now.Year, 9, 15);
+    public static DateTime AnniversaryDate() => new(DateTime.Now.Year, 9, 15);
 }
